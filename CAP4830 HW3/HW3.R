e7d14c33d83ae327676a94ebe3b0449f20a059a6
Vale-Ay <102432143+Vale-Ay@users.noreply.github.com>
2023-08-23 14:24:18 -0400

    Add files via upload

---
+++ CAP4830 HW3/HW3.R
@@ -0,0 +1,130 @@
+#=============================================================================
+# PROGRAMMER: Valeria Aybar
+# PANTHER ID: 6181336
+#
+# CLASS: CAP4830
+# SECTION: RVC 1231
+# SEMESTER: Spring 2023
+# CLASSTIME: Online
+# CERTIFICATION: I understand FIU’s academic policies, and I certify that this
+# work is my own and that none of it is the work of any other person
+#=============================================================================
+
+rm(list = ls()) #clearing all objects from session
+
+library(triangle)
+
+#1 create data frame named inputs with column names (q1, q2, p,s)
+inputs <- data.frame(q1 = rtriangle(2000, a = 0, b = 1500, c = 1200),
+                     q2 = rtriangle(2000, a = 0, b = 3500, c = 1000),
+                     p = rtriangle(2000, a = 10, b = 17.50, c = 12.50),
+                     s = rexp(2000, 10))
+
+#2 plot histograms in a single window
+par(mfrow=c(4, 1)) 
+options(scipen = 999)
+
+hist(inputs$q1)
+hist(inputs$q2)
+hist(inputs$p)
+hist(inputs$s)
+
+#3 create data frame named outputs with a column name value
+
+outputs<- data.frame(value = (2700 - inputs$q1-inputs$q2) *inputs$p - (inputs$s*inputs$p))
+
+#4 run a MonteCarlo Simulation 100 times
+
+for(i in 1:1000){
+  
+  # store the results with outputs 
+  q1 <- sample(inputs$q1 , 1)
+  q2 <- sample(inputs$q2 , 1) 
+  p <- sample(inputs$p , 1) 
+  s <- sample(inputs$s , 1) 
+  
+  value <- ((2700 - q1 - q2) * p - (s*p))
+  
+  # store the results within outputs
+  outputs <- rbind(outputs, data.frame(value))
+}
+
+#5 plot histogram outputs$value
+hist(outputs$value)
+
+#6 create empirical CDF of outputs$value and plot 
+
+empiricalCDF <- ecdf(outputs$value)
+plot(empiricalCDF)
+
+#7 calculate the P0,P10... and output values on R-Console
+quantile(outputs$value, probs = c(seq(0,1,by = 0.1)))
+
+#8 find P20 to P80 interval values 
+
+px <- data.frame (values = quantile(outputs$value, probs = c(seq(0,1,by = 0.1))))
+
+sprintf("Interval of Interest: [ %.2f , %.2f ]",  px$values[3], px$values[9])
+
+#9 create matrix named storage that stores 100 samples
+
+storage <- matrix(ncol = 100, nrow = 250)
+
+for(i in 1:100){
+  
+  outputData <- data.frame(value = double())
+  for(j in 1:250){
+    q1 <- sample(inputs$q1 , 1)
+    q2 <- sample(inputs$q2 , 1) 
+    p <- sample(inputs$p , 1) 
+    s <- sample(inputs$s , 1) 
+    
+    value <- ((2700 - q1 - q2) * p - (s*p))
+    
+    outputData <- rbind(outputData, data.frame(value))
+  }
+  storage[ , i] <- outputData$value
+}
+
+#10 convert storage matrix into data frame storage 
+
+storage <- data.frame(storage)
+
+#11 create a data frame named cltData
+
+cltData <- data.frame(mean = colMeans(storage))
+
+#12 plot histograms of cltData 
+
+par(mfrow=c(1,1))
+hist(cltData$mean)
+
+#13 Use Shapiro test to check if cltdata is normally distributed 
+
+shapiro.test(cltData$mean)
+
+#14 state if the data in cltData is normally distributed 
+
+sprintf("Considering that the p-value is %.4f it is less than 0.5, which means its significantly different from normal distribution.", 
+        shapiro.test(cltData$mean)$p)
+
+#15 calculate the 80% confidence interval and output the lower and upper bounds 
+
+n <- nrow(cltData)
+cltMean <- mean(cltData$mean)
+cltSD <-sd(cltData$mean)
+
+margin <- qt(0.80, df = n-1)* cltSD/sqrt(n)
+
+lowerBound <- cltMean - margin
+higherBound <- cltMean + margin
+
+sprintf("Interval of Interest: [ %.2f ,  %.2f  ]", lowerBound, higherBound)
+
+
+
+
+
+
+
+
---
+++ CAP4830HW2/HW2.R
@@ -0,0 +1,180 @@
+# NAME: Valeria Aybar
+# PANTHER ID: 6181336
+# CERTIFICATION: I understand FIU’s academic policies, and I certify that this
+# work is my own and that none of it is the work of any other person.
+
+#=============================================================================
+# PROGRAMMER: Valeria Aybar
+# PANTHER ID: 6181336
+#
+# CLASS: CAP4830
+# SECTION: RVC 1231
+# SEMESTER: Spring 2023
+# CLASSTIME: Online
+# CERTIFICATION: I understand FIU’s academic policies, and I certify that this
+# work is my own and that none of it is the work of any other person
+#=============================================================================
+
+rm(list = ls()) #clearing all objects from session
+
+
+# 1) Read excel file and store into modelData
+  library("xlsx")   
+  modelData <-read.xlsx(file.choose(), 1) 
+
+
+
+#2) Output the names of the modelData
+  names(modelData)
+
+
+
+#3) create model1
+  model1 <- lm(UNRATE_PCH ~ DFII10_PCH + CPILFESL_PCH + XTEITT01CNM156S_PCH 
+              + DCOILWTICO_PCH + PCOPPUSDM_PCH +  PCE_PCH 
+              + WPU101_PCH +  GPDIC1_PCH +  RRVRUSQ156N_PCH, data = modelData)
+
+  summary(model1)
+
+
+
+#5) Plot model1's residual Density Function
+  library(e1071)
+  plot(density(model1$residuals), 
+      main="Density Plot: Model 1 Residuals", 
+      ylab="Frequency", 
+      sub=paste("Skewness:", round(e1071::skewness(model1$residuals), 2))) 
+
+
+
+#6) Check model1's residual normality using Shapiro Test
+  shapiro.test(model1$residuals)
+  summary(model1)
+
+
+#7)Create model2 removing regressors with p-value >0.55
+  model2 <- lm(UNRATE_PCH ~ DFII10_PCH + XTEITT01CNM156S_PCH + DCOILWTICO_PCH
+             +PCOPPUSDM_PCH + PCE_PCH + WPU101_PCH + GPDIC1_PCH, data = modelData)
+
+  summary(model2)
+             
+
+#9) Calculate prediction accuracy and error rate of model2
+
+  set.seed(100)  
+  
+  model2Data <- data.frame(modelData[ , c("UNRATE_PCH" ,"DFII10_PCH", "XTEITT01CNM156S_PCH",
+                                     "DCOILWTICO_PCH", "PCOPPUSDM_PCH", "PCE_PCH", "WPU101_PCH", "GPDIC1_PCH")] )
+
+  # row indices for training data
+  trainingRowIndex <- sample(1:nrow(model2Data), 0.8*nrow(model2Data)) 
+
+  # model training data
+  trainingData <- model2Data[trainingRowIndex, ] 
+
+  # test data
+  testData  <- model2Data[-trainingRowIndex, ] 
+
+  # build the model
+  trainingModel <- lm(UNRATE_PCH ~ DFII10_PCH + XTEITT01CNM156S_PCH + DCOILWTICO_PCH
+                    + PCOPPUSDM_PCH + PCE_PCH + WPU101_PCH + GPDIC1_PCH, data = modelData)
+
+
+  # predict trainingModel on testing data
+  distPred <- predict(trainingModel, testData)  
+
+  # make actuals_predicted data frame
+  actuals_preds <- data.frame(cbind(index = seq(1: nrow(testData)), 
+                                  actuals= testData$UNRATE_PCH,
+                                  predicteds=distPred))  
+
+
+
+  summary(actuals_preds)
+  cor(actuals_preds$actuals,actuals_preds$predicteds) 
+
+#10) Create model3 with only 3 regressors
+
+  model3 <- lm(UNRATE_PCH ~ PCOPPUSDM_PCH + WPU101_PCH + GPDIC1_PCH, data = modelData)
+  
+  summary(model3)
+
+
+#11) Create model4 using manual sampling with a training set of 60% of the data and a testing set of 40%
+
+  # setting seed to reproduce results of random sampling
+  set.seed(100)  
+  
+  # row indices for training data
+  model4RowIndex <- sample(1:nrow(finalData), 0.6*nrow(finalData)) 
+  
+  # model training data
+  trainingData <- modelData[model4RowIndex , ] 
+  
+  # test data
+  testData  <- modelData[-model4RowIndex , ] 
+  
+  # building model4
+  model4 <- lm(UNRATE_PCH ~ DFII10_PCH + CPILFESL_PCH + XTEITT01CNM156S_PCH 
+               + DCOILWTICO_PCH + PCOPPUSDM_PCH +  PCE_PCH 
+               + WPU101_PCH +  GPDIC1_PCH +  RRVRUSQ156N_PCH, data = trainingData)
+  
+  summary(model4)
+  
+
+#12) Using model4, predict the values on the 40% testing set and store results in the distPred variable
+  
+  # predict model4 on testing data
+  distPred <- predict(model4, testData)
+  
+  # print beginning of distPred
+  head(distPred)
+
+
+#13) Using model4 calculate prediction accuracy and error rates then use ggplot to show actual vs predicted
+
+  # make actuals_predicted data frame
+  actuals_preds <- data.frame(cbind(index = seq(1: nrow(testData)), 
+                                    actuals= testData$UNRATE_PCH,
+                                    predicteds=distPred))  
+  
+  
+  # summary of actuals_preds
+  summary(actuals_preds)
+  
+  # If the correlation accuracy is higher, it means that the actuals and predicted 
+  # values have similar directional movement
+  cor(actuals_preds$actuals,actuals_preds$predicteds) 
+  
+  library(ggplot2)
+  
+  # create gg plot with actuals and predicted values
+  gg <- ggplot(data = actuals_preds, aes(index))  + 
+    geom_point(aes(y = actuals), color = "red") + 
+    geom_point(aes(y = predicteds), color = "blue") +
+    labs( title = "Actual vs Predicted Values")
+  
+  # print out gg plot
+  gg
+  
+
+#14) Run a k-fold cross validation with k=10
+
+  library(caret)
+  
+  # create the control with the 10 k-fold
+  controlled <- trainControl(method = "cv", number = 10)
+  
+  #model using the controlled k-fold
+  k10_model <- train(UNRATE_PCH ~ DFII10_PCH + CPILFESL_PCH + XTEITT01CNM156S_PCH 
+                  + DCOILWTICO_PCH + PCOPPUSDM_PCH +  PCE_PCH 
+                  + WPU101_PCH +  GPDIC1_PCH +  RRVRUSQ156N_PCH, 
+                  data = modelData, method = "lm", trControl = controlled)
+  
+  # print the k model
+  print(k10_model)
+
+  
+  
+  
+  
